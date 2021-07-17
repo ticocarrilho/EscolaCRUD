f160adb1e8108f217bee7236e682b90a9e771453
Thiago Carrilho <ticocarrilho@gmail.com>
2021-07-16 21:09:36 -0300

    add docker compose support

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 class AppController {
+  clientPath = path.join(__dirname, '..', 'client');
 
   constructor() {
     this.express = express();
@@ -17,12 +19,21 @@ class AppController {
       this.express.use(cors({
         origin: '*'
       }));
+    } else if(process.env.NODE_ENV === 'production') {
+      this.express.use(cors({
+        credentials: true,
+        origin: 'https://escola.carrilho.tech',
+        optionsSuccessStatus: 200
+      }));
     }
     
     this.express.use(morgan('common'));
     this.express.use(express.json());
   }
   routes() {
+    if(process.env.NODE_ENV === 'production') {
+      this.express.use(express.static(this.clientPath));
+    }
     this.express.use(routes);
   }
 }
